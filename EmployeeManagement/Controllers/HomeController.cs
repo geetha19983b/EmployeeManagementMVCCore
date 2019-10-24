6eaf4d27946b747608256a545c0309f7485663b2
geetha19983b <geethainfy@gmail.com>
2019-10-24 17:21:29 +0530

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        // Inject IEmployeeRepository using Constructor Injection
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public ViewResult Index()
+        {
+            // retrieve all the employees
+            var model = _employeeRepository.GetAllEmployees();
+            // Pass the list of employees to the view
+            return View(model);
+        }
+        public ViewResult Details()
+        {
+            //Employee model = _employeeRepository.GetEmployee(1);
+
+            //ViewBag.PageTitle = "Employee Details";
+
+            //return View(model);
+            // Instantiate HomeDetailsViewModel and store Employee details and PageTitle
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "Employee Details"
+            };
+
+            // Pass the ViewModel object to the View() helper method
+            return View(homeDetailsViewModel);
+
+        }
+    }
+}
---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        // Notice we are using Dependency Injection here
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //to get the hosting process name
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response
+            //    .WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+            //});
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync(_configuration["MyKey"]);
+            //});
+            // Add Default Files Middleware
+            //app.UseDefaultFiles();
+            // Add Static Files Middleware
+            //app.UseStaticFiles();
+            //app.Use(async (context, next) =>
+            //{
+            //    logger.LogInformation("MW1: Incoming Request");
+            //    await next();
+            //    logger.LogInformation("MW1: Outgoing Response");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    logger.LogInformation("MW2: Incoming Request");
+            //    await next();
+            //    logger.LogInformation("MW2: Outgoing Response");
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("MW3: Request handled and response produced");
+            //    logger.LogInformation("MW3: Request handled and response produced");
+            //});
+        }
+   
+    }
+}
