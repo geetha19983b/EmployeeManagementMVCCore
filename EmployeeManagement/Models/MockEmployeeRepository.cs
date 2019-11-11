9b49d93f953dad38219d6040972fdac1ac7693ca
geetha <geethainfy@gmail.com>
2019-11-11 02:31:17 -0800

    till transient 42 slide tutorial

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -13,9 +13,9 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
         {
-            new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@pragimtech.com" },
-            new Employee() { Id = 2, Name = "John", Department = "IT", Email = "john@pragimtech.com" },
-            new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "sam@pragimtech.com" },
+            new Employee() { Id = 1, Name = "Mary", Department =Dept.HR, Email = "mary@pragimtech.com" },
+            new Employee() { Id = 2, Name = "John", Department =Dept.IT, Email = "john@pragimtech.com" },
+            new Employee() { Id = 3, Name = "Sam", Department = Dept.IT, Email = "sam@pragimtech.com" },
         };
         }
 
@@ -26,6 +26,12 @@ namespace EmployeeManagement.Models
         public IEnumerable<Employee> GetAllEmployees()
         {
             return _employeeList;
+        }
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
         }
     }
 }
